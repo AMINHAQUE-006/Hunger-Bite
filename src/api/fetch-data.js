00a44f668198405fbc67949a047352f01e79b1e3
amin <aminhaque5006@gmail.com>
2024-08-18 01:11:22 +0530

    fucntion updated

--- src/api/fetch-data.js
+++ src/api/fetch-data.js
@@ -1,8 +1,11 @@
 import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
-    const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
-    const data = await response.json();
-
-    res.status(200).json(data);
+export default async (req, res) => {
+    try {
+        const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.518006&lng=88.383206&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
 };
