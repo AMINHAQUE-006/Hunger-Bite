d6f2741386a5b2213e5ad8784676a42aa6cf8630
amin <aminhaque5006@gmail.com>
2024-08-18 01:07:23 +0530

    node fetch added

---
+++ src/api/fetch-data.js
@@ -0,0 +1,8 @@
+import fetch from 'node-fetch';
+
+module.exports = async (req, res) => {
+    const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    const data = await response.json();
+
+    res.status(200).json(data);
+};
