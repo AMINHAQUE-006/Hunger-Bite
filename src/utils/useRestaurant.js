59a0efcbaad17a7ba3432e54e2d7d71a443c907a
amin <aminhaque5006@gmail.com>
2024-08-18 01:02:18 +0530

    api path change

--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -9,8 +9,6 @@ function useRestaurant() {
     }, [])
     
     async function getRestaurants() {
-        const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-        const targetUrl = GET_RESTAURANT_URL;
         fetch(proxyUrl + targetUrl)
         .then(response => response.json())
         .then(res => setAllRestaurant(res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants))
